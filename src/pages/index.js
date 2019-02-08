206f8af7bc6160043d6fd8310ff61dfcf0a9fda6
salviolorenzo <salviolorenzo@gmail.com>
2019-02-08 13:05:00 -0500

    adds simple component structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,21 @@
-import React from "react"
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import Body from '../components/Body';
+import Footer from '../components/Footer';
 
-export default () => <div>Hello world!</div>
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        Eric Salvio
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+    );
+  }
+}
